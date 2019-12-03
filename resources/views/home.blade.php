6170e8ea81b0bb83e7228bd36ffcdfbadb367a08
aqshalzakki <kecoatempur2@gmail.com>
2019-12-03 22:55:19 +0700

    mengubah halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="{{ asset('images/profile.jpg') }}" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="">
+                <h1>aqshalzakki</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>1</strong> posts</div>
+                <div class="pr-5"><strong>141</strong> followers</div>
+                <div class="pr-5"><strong>139</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">yourdisturber</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque reiciendis vel ratione laborum commodi aliquid nisi, iusto voluptates ad, deserunt consequatur.</div>
+            <div><a href="https://github.com/aqshalzakki">My github account</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fbdo5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/19985093_235710903614081_1709185430898868224_n.jpg?_nc_ht=instagram.fbdo5-1.fna.fbcdn.net&_nc_cat=105&oh=177de601831ddf2eccf4645274ad98c9&oe=5E7473A2" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="http://news.efinancialcareers.com/binaries/content/gallery/efinancial-careers/articles/2019/03/programmer.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--ZmPcIbAW--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dzone.com/storage/temp/12334613-971.jpg" class="w-100">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img class="pr-3 mb-1" src="{{ asset('images/instagram.svg') }}" style="height: 22px; border-right: 1px solid #333"></div>
+                    <div class="pl-3">{{ config('app.name') }}</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
